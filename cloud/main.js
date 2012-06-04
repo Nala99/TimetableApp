0b53fbf56a2a3d6be2e7f47e8647bd62652aca36
alanjoneill18@gmail.com <alanjoneill18@gmail.com>
2012-06-04 21:19:50 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -23,7 +23,7 @@ exports.getLFeeds=function(params,callback){
       return callback(null,err);
     }
     else{
-      var fdataList = res.list;
+      var fdataList = res.children;
       return callback(null,fdataList);
     }
   });
