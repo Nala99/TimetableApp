7a9deb17d18cc600f8af09c730814060fd3af818
alanjoneill18@gmail.com <alanjoneill18@gmail.com>
2012-06-04 16:40:20 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -16,7 +16,7 @@ of results retrieved will be 10
 */
 exports.getLFeeds=function(params,callback){
   var ldata = $fh.feed({
-  "link":"http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=witlibraries",
+  "link":"http://www.feedhenry.com/feed",//"http://api.twitter.com/1/statuses/user_timeline.rss?screen_name=witlibraries",
   "list-max": 10
   },function(err,res){
     if(err){
