b6622cabf6879f807998382a6e2d245c582badb8
alanjoneill18@gmail.com <alanjoneill18@gmail.com>
2012-06-05 00:18:21 +0000

    update file /client/default/js/init.js

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -167,7 +167,7 @@ function showFeed(name){
       act:'getLFeeds'
     },function(res){
       console.log(res);
-      console.log(res.fields.title);
+      console.log(res[2].fields.title);
       
         $("<p>" + res[2].data + "</p>").appendTo("#contentText");
     
